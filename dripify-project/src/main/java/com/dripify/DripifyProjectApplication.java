99a46d58a854a972ea5ea145a68221e63289e615
Andjelina Kumanova <angelinakumanova@gmail.com>
2024-12-25 12:07:13 +0200

    added project architecture

---
+++ dripify-project/src/main/java/com/dripify/DripifyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dripify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DripifyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DripifyProjectApplication.class, args);
+    }
+
+}
---
+++ dripify-project/src/test/java/com/dripify/DripifyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dripify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DripifyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
