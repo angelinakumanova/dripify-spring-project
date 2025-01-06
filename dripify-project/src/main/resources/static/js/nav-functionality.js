c1b9e46d5fb649cd248edf9cb53870cce65b50cc
Andjelina Kumanova <angelinakumanova@gmail.com>
2025-01-06 14:56:52 +0200

    Refactor nav js functionality

--- dripify-project/src/main/resources/static/js/nav-functionality.js
+++ dripify-project/src/main/resources/static/js/nav-functionality.js
@@ -1,110 +1,127 @@
- // MOBILE MENU FUNCTIONALITY
-    // Get the elements
-    const mobileMenuButton = document.querySelector('.mobile-menu button.hamburger'); // Hamburger button
-    const mobileMenu = document.querySelector('div[role="dialog"]'); // Mobile menu
+function bindToggleAnimation(trigger, target) {
+  const triggerElement = document.getElementById(trigger);
+  const targetElement = document.getElementById(target);
 
-    const closeButton = mobileMenu.querySelector('button'); // Close button inside mobile menu
-    const backdrop = mobileMenu.querySelector('.fixed'); // The backdrop to close when clicked
+  if (!triggerElement || !targetElement) {
+    console.warn(`Missing element: ${trigger || target}`);
+    return;
+  }
 
+  triggerElement.addEventListener('click', () => toggleOpacityAnimation(targetElement));
+  targetElement.addEventListener('mouseleave', () => toggleOpacityAnimation(targetElement));
+}
 
-    // Function to open the menu
-    const openMenu = () => {
-      mobileMenu.classList.remove('hidden'); // Show the mobile menu
-      backdrop.classList.remove('hidden'); // Show the backdrop
-    };
-
-    // Function to close the menu
-    const closeMenu = () => {
-      mobileMenu.classList.add('hidden'); // Hide the mobile menu
-      backdrop.classList.add('hidden'); // Hide the backdrop
-    };
-
-    // Event listener to open the menu
-    mobileMenuButton.addEventListener('click', openMenu);
-
-    // Event listener to close the menu
-    closeButton.addEventListener('click', closeMenu);
-    backdrop.addEventListener('click', closeMenu);
+bindToggleAnimation('flyout-menu-button', 'flyout-menu');
+bindToggleAnimation('profile-name', 'profile-dropdown');
 
 
+function toggleOpacityAnimation(element) {
+  element.classList.toggle('opacity-0');
+  element.classList.toggle('opacity-100');
 
-    // SHOP BUTTON FUNCTIONALITY 
-    const mobileCloseButton = document.getElementById("mobile-close-button");
-    const mobileMenuShopButton = document.getElementById("shop-mobile-button");
-    const mobileReturnButton = document.getElementById("mobile-return-button");
-    const mobileNav = document.getElementById("mobile-nav");
-    const mobileCategoryMenu = document.getElementById("mobile-categories");
+  element.classList.toggle('pointer-events-none');
+  element.classList.toggle('pointer-events-auto');
+}
 
-    function toggleMenu() {
-      mobileNav.classList.toggle('hidden');
-      mobileCategoryMenu.classList.toggle('hidden');
 
-      mobileCloseButton.classList.toggle('hidden');
-      mobileReturnButton.classList.toggle('hidden');
+// Utility Functions
+const toggleVisibility = (element) => {
+  element.classList.toggle('hidden');
+};
+
+const toggleClasses = (element, classesToAdd, classesToRemove) => {
+  element.classList.add(...classesToAdd);
+  element.classList.remove(...classesToRemove);
+};
+
+// MOBILE MENU FUNCTIONALITY
+const mobileMenuButton = document.querySelector('.mobile-menu button.hamburger');
+const mobileMenu = document.querySelector('div[role="dialog"]');
+const closeButton = mobileMenu.querySelector('button');
+const backdrop = mobileMenu.querySelector('.fixed');
+
+const toggleMenu = () => {
+  toggleVisibility(mobileMenu);
+  toggleVisibility(backdrop);
+};
+
+mobileMenuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+backdrop.addEventListener('click', toggleMenu);
+
+// SHOP BUTTON FUNCTIONALITY
+const mobileCloseButton = document.getElementById("mobile-close-button");
+const mobileMenuShopButton = document.getElementById("shop-mobile-button");
+const mobileReturnButton = document.getElementById("mobile-return-button");
+const mobileNav = document.getElementById("mobile-nav");
+const mobileCategoryMenu = document.getElementById("mobile-categories");
+
+const toggleMenuViewCategory = () => {
+  toggleVisibility(mobileNav);
+  toggleVisibility(mobileCategoryMenu);
+  toggleVisibility(mobileCloseButton);
+  toggleVisibility(mobileReturnButton);
+};
+
+mobileMenuShopButton.addEventListener('click', () => toggleMenuViewCategory());
+mobileReturnButton.addEventListener('click', () => toggleMenuViewCategory());
+
+
+// CATEGORY VISIBILITY FUNCTIONALITY
+const womenTab = document.getElementById('women-tab');
+const menTab = document.getElementById('men-tab');
+
+const womenTabMobile = document.getElementById('women-tab-mobile');
+const menTabMobile = document.getElementById('men-tab-mobile');
+
+womenTab.addEventListener('click', () => toggleCategoryVisibility(womenTab));
+menTab.addEventListener('click', () => toggleCategoryVisibility(menTab));
+
+womenTabMobile.addEventListener('click', () => toggleCategoryVisibility(womenTabMobile));
+menTabMobile.addEventListener('click', () => toggleCategoryVisibility(menTabMobile));
+
+const toggleCategoryVisibility = (current) => {
+  const { categories: womenCategories, tab: womenTab, underline: womenUnderline } = getCategoryElements('women', current.id.includes('mobile'));
+  const { categories: menCategories, tab: menTab, underline: menUnderline } = getCategoryElements('men', current.id.includes('mobile'));
+
+  if (current.getAttribute('category') === 'women') {
+    toggleVisibility(womenCategories, true);
+    toggleVisibility(menCategories, false);
+    toggleClasses(womenTab, ['font-bold'], ['font-semibold']);
+    toggleClasses(menTab, ['font-semibold'], ['font-bold']);
+    toggleClasses(womenUnderline, ['w-full'], []);
+    toggleClasses(menUnderline, [], ['w-full']);
+  } else if (current.getAttribute('category') === 'men') {
+    toggleVisibility(menCategories, true);
+    toggleVisibility(womenCategories, false);
+    toggleClasses(menTab, ['font-bold'], ['font-semibold']);
+    toggleClasses(womenTab, ['font-semibold'], ['font-bold']);
+    toggleClasses(menUnderline, ['w-full'], []);
+    toggleClasses(womenUnderline, [], ['w-full']);
+  }
+};
+
+function getCategoryElements(category, isMobile = false) {
+  const womenCategories = document.getElementById(isMobile ? 'women-categories-mobile' : 'women-categories');
+  const menCategories = document.getElementById(isMobile ? 'men-categories-mobile' : 'men-categories');
+  const womenTab = document.getElementById(isMobile ? 'women-tab-mobile' : 'women-tab');
+  const menTab = document.getElementById(isMobile ? 'men-tab-mobile' : 'men-tab');
+
+  if (category === 'women')
+    return {
+      categories: womenCategories,
+      tab: womenTab,
+      underline: womenTab.children[0],
     }
 
-    function showCategoryMobile(category) {
-      const womenCategories = document.getElementById('women-categories-mobile');
-      const menCategories = document.getElementById('men-categories-mobile');
-
-      const womenTab = document.getElementById('women-tab-mobile');
-      const menTab = document.getElementById('men-tab-mobile');
-
-
-      toggleCategoryVisibility(category, womenCategories, menCategories, womenTab, menTab)
-    }
+  return {
+    categories: menCategories,
+    tab: menTab,
+    underline: menTab.children[0],
+  }
+};
 
 
 
-// LARGER SCREENS
-    function showCategory(category) {
-      // Get category elements
-      const womenCategories = document.getElementById('women-categories');
-      const menCategories = document.getElementById('men-categories');
-
-      const womenTab = document.getElementById('women-tab');
-      const menTab = document.getElementById('men-tab');
 
 
-      toggleCategoryVisibility(category, womenCategories, menCategories, womenTab, menTab)
-    }
-
-    function toggleCategoryVisibility(category, womenCategories, menCategories, womenTab, menTab) {
-      const womenTabUnderline = womenTab.children[0];
-      const menTabUnderline = menTab.children[0];
-
-      if (category === 'women') {
-        // Show women's categories and update tab styles
-        womenCategories.classList.remove('hidden');
-        menCategories.classList.add('hidden');
-
-        womenTab.classList.add('font-bold');
-        womenTab.classList.remove('font-semibold');
-        menTab.classList.replace('font-bold', 'font-semibold');
-
-        womenTabUnderline.classList.add('w-full');
-        menTabUnderline.classList.replace('w-full', 'w-0');
-      } else if (category === 'men') {
-        // Show men's categories and update tab styles  
-        menCategories.classList.remove('hidden');
-        womenCategories.classList.add('hidden');
-
-        menTab.classList.add('font-bold');
-        menTab.classList.remove('font-semibold');
-        womenTab.classList.replace('font-bold', 'font-semibold');
-
-        menTabUnderline.classList.add('w-full');
-        womenTabUnderline.classList.replace('w-full', 'w-0');
-      }
-    }
-
-
-    function toggleOpacityAnimation(element) {
-      element.classList.toggle('opacity-0');
-      element.classList.toggle('opacity-100');
-
-      element.classList.toggle('pointer-events-none');
-      element.classList.toggle('pointer-events-auto');
-    }
-
-    
